1561e0cea48c7b9b3bd7032d1a844fd33635da61
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-02-01 19:13:44 -0300

    refactor: CreateUserController

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,5 +1,6 @@
 import { CreateUserUseCase } from '../use-cases/create-user.js';
 import validator from 'validator';
+import { badRequest, created, serverError } from './helpers.js';
 
 export class CreateUserController {
   async execute(httpRequest) {
@@ -9,50 +10,31 @@ export class CreateUserController {
       const requiredFields = ['first_name', 'last_name', 'email', 'password'];
       for (const field of requiredFields) {
         if (!params[field] || params[field].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing param: ${field}`,
-            },
-          };
+          return badRequest({ message: `Missing param: ${field}` });
         }
       }
 
       const passwordIsValid = params.password.length > 6;
-
       if (!passwordIsValid) {
-        return {
-          statusCode: 400,
-          body: {
-            errorMessage: 'Password must be at least 6 characters.',
-          },
-        };
+        return badRequest({
+          message: 'Password must be at least 6 characters.',
+        });
       }
 
       const emailIsValid = validator.isEmail(params.email);
       if (!emailIsValid) {
-        return {
-          statusCode: 400,
-          body: {
-            errorMessage: 'Invalid e-mail. Please provide a valid one.',
-          },
-        };
+        return badRequest({
+          message: 'Invalid e-mail. Please provide a valid one.',
+        });
       }
 
       const createUserUseCase = new CreateUserUseCase();
       const createdUser = await createUserUseCase.execute(params);
-      return {
-        statusCode: 201,
-        body: createdUser,
-      };
+      console.log(createdUser);
+      return created(createdUser);
     } catch (err) {
       console.error(err);
-      return {
-        statusCode: 500,
-        body: {
-          message: 'Interval server error',
-        },
-      };
+      return serverError();
     }
   }
 }
