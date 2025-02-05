2cdda4fe6985c51019efb2dd62fb0758bd376a3a
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-02-05 20:31:18 -0300

    feat: add sql function to get user balance

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -10,9 +10,16 @@ const __dirname = path.dirname(__filename);
 const execMigrations = async () => {
   const client = await pool.connect();
   try {
-    const filePath = path.join(__dirname, './01-init.sql');
-    const script = fs.readFileSync(filePath, 'utf-8');
-    client.query(script);
+    const files = fs
+      .readdirSync(__dirname)
+      .filter((file) => file.endsWith('.sql'));
+
+    for (const file of files) {
+      const filePath = path.join(__dirname, file);
+      const script = fs.readFileSync(filePath, 'utf-8');
+      await client.query(script);
+      console.log(`Migration: ${file} executed successfully`);
+    }
 
     console.log('Migrations executed successfully');
   } catch (err) {
